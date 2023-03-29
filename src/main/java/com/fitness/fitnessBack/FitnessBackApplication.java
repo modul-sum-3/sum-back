12543d9aa4517eca53ecd930d56bfb6977652bd5
236558 Kazusek Karol <236558@edu.p.lodz.pl>
2023-03-29 13:08:56 +0200

    init

---
+++ src/main/java/com/fitness/fitnessBack/FitnessBackApplication.java
@@ -0,0 +1,13 @@
+package com.fitness.fitnessBack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitnessBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitnessBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fitness/fitnessBack/FitnessBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fitness.fitnessBack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitnessBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
