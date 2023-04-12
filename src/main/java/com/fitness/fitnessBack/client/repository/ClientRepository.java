66c9cbdc52b939ee108d5116cecc80079a83eafc
236547 Janicki Marcin <236547@edu.p.lodz.pl>
2023-04-12 09:50:31 +0200

    Client Object

---
+++ src/main/java/com/fitness/fitnessBack/client/repository/ClientRepository.java
@@ -0,0 +1,9 @@
+package com.fitness.fitnessBack.client.repository;
+
+import com.fitness.fitnessBack.client.model.Client;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ClientRepository extends JpaRepository<Client,Long> {
+}
