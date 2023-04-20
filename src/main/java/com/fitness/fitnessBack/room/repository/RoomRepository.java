3032e26e3976eb20619e85d47a2b8d265fc10a37
236547 Janicki Marcin <236547@edu.p.lodz.pl>
2023-04-20 17:11:05 +0200

    Adding Room

---
+++ src/main/java/com/fitness/fitnessBack/room/repository/RoomRepository.java
@@ -0,0 +1,9 @@
+package com.fitness.fitnessBack.room.repository;
+
+import com.fitness.fitnessBack.room.model.Room;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoomRepository extends JpaRepository<Room, Long> {
+}
